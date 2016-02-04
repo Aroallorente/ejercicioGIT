132a62c4d199eb9f1cf88443d38dcbb140e97f02
Arranque 1 <Arranque 1>
2016-02-04 13:57:37 +0100

    Subo clientes.sql

---
+++ clientes.sql
@@ -0,0 +1,24 @@
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `nif` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `dirección` varchar(45) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  PRIMARY KEY (`nif`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
